39f5d18c2d10d9e89e66748444e28ef3a7f91efc
ChaseOB <chas3.0brien@gmail.com>
2021-01-27 23:14:28 -0500

    organization/cleanup

---
+++ TSA Game 2020-21v2/Assets/Scripts/MatthewK Scripts/SourceReciever.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SourceReciever : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+
+        if (other.GetComponent<Electricity>() != null)
+        {
+            if (other.GetComponent<Electricity>().source)
+            {
+                GetComponent<Electricity>().source = true;
+                GetComponent<Electricity>().Power();
+                Destroy(other);
+            }
+        }
+    }
+}
