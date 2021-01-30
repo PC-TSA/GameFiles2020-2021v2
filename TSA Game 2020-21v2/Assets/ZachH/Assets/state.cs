f0b537dd6ace257a96f7cd2753d2c7f7bc70aa26
zachHarpaz <61989294+zachHarpaz@users.noreply.github.com>
2021-01-29 21:01:09 -0500

    Charcter and cut scenes

---
+++ TSA Game 2020-21v2/Assets/ZachH/Assets/state.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class state : MonoBehaviour
+{
+    public bool matters;
+    public bool wireState = false;
+    public int wrongRotation;
+    public bool isSelected;
+    wirePuzzleScript wire;
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.rotation.y == wrongRotation && matters==true)
+        {
+            wireState = true;
+        }
+        
+    }
+}
