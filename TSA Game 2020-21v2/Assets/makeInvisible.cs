c2b4520d01145ea88da0dc396f8fdef43ab8488c
ChaseOB <chas3.0brien@gmail.com>
2021-01-30 19:29:21 -0500

    invisible walls in forest, magnet container

---
+++ TSA Game 2020-21v2/Assets/makeInvisible.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeInvisible : MonoBehaviour
+{
+    public Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
