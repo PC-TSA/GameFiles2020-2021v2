717c34c09199133062c58980a966286747d17f4f
zachHarpaz <61989294+zachHarpaz@users.noreply.github.com>
2021-02-01 20:06:20 -0500

    Exit from puzzle

---
+++ TSA Game 2020-21v2/Assets/activate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activate : MonoBehaviour
+{
+    public GameObject wirePuzzle;
+    public GameObject puzzleCamera;
+    public GameObject playerCamera;
+    // Update is called once per frame
+    private void Start()
+    {
+        activatePuzzle();
+    }
+   
+
+    public void activatePuzzle()
+    {
+        wirePuzzle.GetComponent<wirePuzzleScript>().enabled = true;
+        puzzleCamera.SetActive(true);
+        playerCamera.SetActive(false);
+    }
+    public void deactivatePuzzle()
+    {
+        Debug.Log("deActive");
+        wirePuzzle.GetComponent<wirePuzzleScript>().enabled = false;
+        puzzleCamera.SetActive(false);
+        playerCamera.SetActive(true);
+
+    }
+}
