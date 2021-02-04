f3c240ed468b22661f02e18e90fec0944f628110
zachHarpaz <61989294+zachHarpaz@users.noreply.github.com>
2021-02-03 19:13:18 -0500

    magnet done

--- TSA Game 2020-21v2/Assets/ZachH/Assets/laserScript2.cs
+++ TSA Game 2020-21v2/Assets/ZachH/Assets/laserScript2.cs
@@ -10,14 +10,55 @@ public class laserScript2 : MonoBehaviour
     public Transform magnet;
     public LayerMask magnetLayer;
     public GameObject magnetEmit;
+    public float range;
+    public float maxRange;
+    public float minRange;
+    public float liveMaxRange;
+    public float liveMinRange;
+    public Vector3 originTrans;
+    public float particleScale;
+    public float particleStart;
+    public float liveCount = 1;
     // Update is called once per frame
+    private void Start()
+    {
+         originTrans = transform.localPosition;
+        particleStart = magnetEmit.GetComponent<ParticleSystem>().startLifetime;
+    }
     void FixedUpdate()
     {
-       // holdObject.position = transform.position;
-
+        // holdObject.position = transform.position;
         RaycastHit hit;
+        
+        range = Input.mouseScrollDelta.y;
+        range = Mathf.Clamp(range, minRange, maxRange);
+
+        liveCount += range;
+
+        if (liveCount > liveMaxRange)
+        {
+            maxRange = 0;
+            minRange = -1;
+
+        }else if (liveCount < liveMinRange)
+        {
+            minRange = 0;
+            maxRange = 1;
+
+        }
+        else
+        {
+            maxRange = 1;
+            minRange = -1;
+        }
+
+        transform.position += magnetEmit.transform.forward * range;
+
+        Debug.Log(range);
         if (Input.GetMouseButton(0))
         {
+            magnetEmit.GetComponent<ParticleSystem>().startLifetime += range * particleScale;
+
             magnetEmit.SetActive(true);
             if (Physics.Raycast(magnet.position, magnet.TransformDirection(Vector3.forward), out hit, Vector3.Distance(magnet.position, transform.position), magnetLayer))
             { 
@@ -34,7 +75,12 @@ public class laserScript2 : MonoBehaviour
         }
         else
         {
+            transform.localPosition = originTrans;
+            range = 0;
+            magnetEmit.GetComponent<ParticleSystem>().startLifetime = particleStart;
+            liveCount = 1;
             magnetEmit.SetActive(false);
+
         }
         
     }
