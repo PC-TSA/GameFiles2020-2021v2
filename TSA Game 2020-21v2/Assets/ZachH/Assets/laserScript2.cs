96df63614046e3e1e3d370d4e8c6d7a3bccd51a5
AMindToThink <61801493+AMindToThink@users.noreply.github.com>
2021-04-26 20:41:14 -0400

    Magnet Clipping
    I believe I have solved at least some of the problems of magnet clipping. The storage crates are still weird,.
    For reasons that are probably my fault, we now have confusingly two player prefabs. I am not sure which we are using. We need to clear this up.

--- TSA Game 2020-21v2/Assets/ZachH/Assets/laserScript2.cs
+++ TSA Game 2020-21v2/Assets/ZachH/Assets/laserScript2.cs
@@ -9,6 +9,7 @@ public class laserScript2 : MonoBehaviour
     public Transform endPoint;
     public Transform magnet;
     public LayerMask magnetLayer;
+    private int invisibleLayer;
     public GameObject magnetEmit;
     public float range;
     public float maxRange;
@@ -19,13 +20,15 @@ public class laserScript2 : MonoBehaviour
     public float particleScale;
     public float particleStart;
     public float liveCount = 1;
-
+    
+    private GameObject holding;
     
     // Update is called once per frame
     private void Start()
     {
          originTrans = transform.localPosition;
         particleStart = magnetEmit.GetComponent<ParticleSystem>().startLifetime;
+        invisibleLayer = LayerMask.GetMask("Invisible");
     }
     void FixedUpdate()
     {
@@ -73,14 +76,16 @@ public class laserScript2 : MonoBehaviour
         transform.position += magnetEmit.transform.forward * range;
 
         //Stuff added by Matthew to stop magnet clipping:
-        int bitLayerMask = ~magnetLayer.value;//This int represents set of layers: all except (that is what the ~ operator does) the magnet layer
-        if (Physics.Raycast(magnet.position, magnet.TransformDirection(Vector3.forward), out hit, Vector3.Distance(magnet.position, transform.position), bitLayerMask))
+        GameObject blockerObject = null;
+        int bitLayerMask = ~(magnetLayer.value + invisibleLayer);//This int represents set of layers: all except (that is what the ~ operator does) the magnet layer and the invisible layer of colliders that are not physics.
+        if (Physics.Raycast(magnet.position, magnet.TransformDirection(Vector3.forward), out hit, liveCount + originTrans.magnitude/*Vector3.Distance(magnet.position, transform.position)*/, bitLayerMask))
         {
             transform.position = hit.point;
+            blockerObject = hit.transform.gameObject;
         }
         else 
         {
-            transform.localPosition = originTrans;
+            transform.position = transform.parent.transform.TransformPoint(originTrans) + liveCount * magnetEmit.transform.forward;
             
         }
         //End Matt stuff
@@ -88,7 +93,7 @@ public class laserScript2 : MonoBehaviour
         Debug.Log(range);
         if (Input.GetMouseButton(0))
         {
-            magnetEmit.GetComponent<ParticleSystem>().startLifetime += range * particleScale;
+            magnetEmit.GetComponent<ParticleSystem>().startLifetime = Vector3.Distance(magnet.position, transform.position) * particleScale/1.05f;
 
             magnetEmit.SetActive(true);
             if (Physics.Raycast(magnet.position, magnet.TransformDirection(Vector3.forward), out hit, Vector3.Distance(magnet.position, transform.position), magnetLayer))
@@ -99,10 +104,14 @@ public class laserScript2 : MonoBehaviour
 
 
                 //hit.collider.transform.position = transform.position;
+                holding = hit.collider.transform.gameObject;
                 Vector3 hitScale = hit.collider.transform.localScale;
-                hit.collider.gameObject.GetComponent<Rigidbody>().MovePosition(transform.position - magnetEmit.transform.forward * Mathf.Max(hitScale.x, Mathf.Max(hitScale.y, hitScale.z)));
+                hit.collider.gameObject.GetComponent<Rigidbody>().MovePosition(transform.position + ((blockerObject != null) ? ((blockerObject.transform.position.y < transform.position.y) ? transform.up * hitScale.y/2 : Vector3.zero) : Vector3.zero)  - magnetEmit.transform.forward * Mathf.Max(hitScale.x, Mathf.Max(hitScale.y, hitScale.z)));
                 //hit.collider.gameObject.GetComponent<Rigidbody>().
                 hit.collider.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                holding.transform.rotation.eulerAngles.Set(holding.transform.rotation.eulerAngles.x, 0f, holding.transform.rotation.eulerAngles.z);
+                hit.collider.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                holding.GetComponent<Rigidbody>().freezeRotation = true;
             }
 
 
@@ -110,10 +119,14 @@ public class laserScript2 : MonoBehaviour
         }
         else
         {
-            transform.localPosition = originTrans;
+            //transform.localPosition = originTrans;
+            if (holding != null) {
+                holding.GetComponent<Rigidbody>().freezeRotation = false;
+                holding = null;
+            }
             range = 0;
             magnetEmit.GetComponent<ParticleSystem>().startLifetime = particleStart;
-            liveCount = 1;
+            //liveCount = 1;
             magnetEmit.SetActive(false);
 
         }
