01f9011ff7cd10f36f8f83dcf7607791db582bff
AMindToThink <61801493+AMindToThink@users.noreply.github.com>
2021-05-12 13:21:21 -0400

    Added Isa's Transition Animation
    Unity doesn't support Gifs, so I turned it into JPEGs using an online converter and found/modified a script to cycle through them. I'll admit that I would have preferred a better solution, but it works.

---
+++ TSA Game 2020-21v2/Assets/GifAnimate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GifAnimate : MonoBehaviour
+{
+
+    public Sprite[] frames;
+
+    public int framesPerSec = 10;
+void Update() {
+    int index = (int)(Time.time * framesPerSec) % frames.Length;
+        //renderer.material.mainTexture = frames[index];
+        GetComponent<Image>().sprite = frames[index];
+}
+}
